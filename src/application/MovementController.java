c99f195d90a2de17820b1c756806b9e73cccaf5b
Rachel Pawliuk <34349409+rpawliuk@users.noreply.github.com>
2020-08-22 15:14:40 -0600

    Adding map fxml and controller

---
+++ src/application/MovementController.java
@@ -0,0 +1,27 @@
+/**
+ * Sample Skeleton for 'MovementView.fxml' Controller Class
+ */
+package application;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MovementController.java {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+}
